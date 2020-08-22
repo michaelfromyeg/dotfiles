d215950dd552b5481314a40004a798e65cb717e2
Michael DeMarco <michaelfromyeg@gmail.com>
2020-08-22 16:09:43 -0600

    Adding powerline support

--- src/Microsoft.PowerShell_profile.ps1
+++ src/Microsoft.PowerShell_profile.ps1
@@ -4,9 +4,15 @@ if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 }
 
+# Git aliases
 function Get-GitStatus { & git status $args }
 New-Alias -Name s -Value Get-GitStatus
 function Set-GitCommit { & git commit -am $args }
 New-Alias -Name c -Value Set-GitCommit
 function Set-GitPush { & git push -u origin $args }
 New-Alias -Name p -Value Set-GitPush
+
+# Powerline features
+Import-Module posh-git
+Import-Module oh-my-posh
+Set-Theme Paradox
