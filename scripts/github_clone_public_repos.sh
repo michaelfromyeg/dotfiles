03fe074e05244281182446ac6e46ff3eadeace37
Michael DeMarco <michaelfromyeg@gmail.com>
2021-01-24 12:52:17 -0800

    updating gh clone scripts

--- scripts/github_clone_public_repos.sh
+++ scripts/github_clone_public_repos.sh
@@ -1,7 +1,14 @@
 # From https://stackoverflow.com/questions/19576742/how-to-clone-all-repos-at-once-from-github
 # Change NAME="..." to your GitHub username
 # ...and then run `sh github_clone_all.sh` wherever you want your repos to be cloned!
-CNTX="users"; NAME="michaelfromyeg"; PAGE=1
+
+if [ $# -ne 1 ]
+  then
+    echo "Missing arguments; usage: sh clone_github_repos.sh GITHUB_USER_NAME"
+    exit 1
+fi
+
+CNTX="users"; NAME="$1"; PAGE=1
 curl "https://api.github.com/$CNTX/$NAME/repos?page=$PAGE&per_page=100" |
   grep -e 'git_url*' |
   cut -d \" -f 4 |
