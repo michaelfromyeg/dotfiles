05191dc24d6daa6e6ce045c14f9874bd56bb22c6
Michael DeMarco <michaelfromyeg@gmail.com>
2021-01-24 12:50:17 -0800

    add script for cloning gh repos

---
+++ ahks/emdash.ahk
@@ -0,0 +1,11 @@
+^-:: ;en dash (150/x96) [CTRL + HYPHEN]
+Send –
+Return
+
+!-:: ;em dash (151/x97) [ALT + HYPHEN]
+Send —
+Return
+
+!^-:: ;bullet (149/x95) [ALT + CTRL + HYPHEN]
+Send •
+Return
---
+++ clone_github_repos.sh
@@ -0,0 +1,9 @@
+# Original code from: https://stackoverflow.com/questions/19576742/how-to-clone-all-repos-at-once-from-github
+
+if [ $# -ne 2 ]
+  then
+    echo "Missing arguments; usage: sh clone_github_repos.sh GITHUB_USER_NAME GITHUB_API_TOKEN"
+    exit 1
+fi
+
+curl -s "https://api.github.com/users/$1/repos?access_token=$2&per_page=1000" | grep -w clone_url | grep -o '[^"]\+://.\+.git' | xargs -L1 git -C ~/code clone 
