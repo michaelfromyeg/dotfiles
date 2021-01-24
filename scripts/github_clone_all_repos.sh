916bc75b6cf8a72fdce53cd5009764373cee212f
Michael DeMarco <michaelfromyeg@gmail.com>
2021-01-24 12:55:45 -0800

    updating scripts to meet new api policies

--- scripts/github_clone_all_repos.sh
+++ scripts/github_clone_all_repos.sh
@@ -6,4 +6,4 @@ if [ $# -ne 2 ]
     exit 1
 fi
 
-curl -s "https://api.github.com/users/$1/repos?access_token=$2&per_page=1000" | grep -w clone_url | grep -o '[^"]\+://.\+.git' | xargs -L1 git -C ~/code clone 
+curl -H "Authorization: token $2" -s "https://api.github.com/users/$1/repos?per_page=1000" | grep -w clone_url | grep -o '[^"]\+://.\+.git' | xargs -L1 git -C ~/code clone 
--- scripts/github_clone_public_repos.sh
+++ scripts/github_clone_public_repos.sh
@@ -1,6 +1,4 @@
-# From https://stackoverflow.com/questions/19576742/how-to-clone-all-repos-at-once-from-github
-# Change NAME="..." to your GitHub username
-# ...and then run `sh github_clone_all.sh` wherever you want your repos to be cloned!
+# Original code from: https://stackoverflow.com/questions/19576742/how-to-clone-all-repos-at-once-from-github
 
 if [ $# -ne 1 ]
   then
@@ -9,7 +7,7 @@ if [ $# -ne 1 ]
 fi
 
 CNTX="users"; NAME="$1"; PAGE=1
-curl "https://api.github.com/$CNTX/$NAME/repos?page=$PAGE&per_page=100" |
+curl "https://api.github.com/$CNTX/$NAME/repos?page=$PAGE&per_page=1000" |
   grep -e 'git_url*' |
   cut -d \" -f 4 |
   xargs -L1 git clone
