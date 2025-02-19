b2ede782a27142957dd19e78dafd17c896bef330
Michael DeMarco <michaelfromyeg@gmail.com>
2025-02-19 01:25:24 -0800

    feat: clean-up

--- scripts/homebrew.sh
+++ scripts/homebrew.sh
@@ -7,4 +7,3 @@ if ! command -v brew >/dev/null 2>&1; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-eval "$(/opt/homebrew/bin/brew shellenv)"
--- scripts/zsh-to-bash.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# Switches you from zsh to bash.
-
-chsh -s /opt/homebrew/bin/bash
