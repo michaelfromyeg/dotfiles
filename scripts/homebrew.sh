d2ce1ebc84ae32991f4f2af20010339bcf337be1
Michael DeMarco <michaelfromyeg@gmail.com>
2026-04-06 17:00:07 -0700

    feat: more things

--- scripts/homebrew.sh
+++ scripts/homebrew.sh
@@ -6,3 +6,5 @@ if ! command -v brew >/dev/null 2>&1; then
   echo "Installing Homebrew..."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
+
+brew install git-town
